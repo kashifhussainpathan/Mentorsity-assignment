2c3880498c96d331fd9a63ee3ddc7c85e30a187b
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-02-05 14:06:08 +0530

    refactor: uncomment func call

--- src/components/symbols.jsx
+++ src/components/symbols.jsx
@@ -16,14 +16,15 @@ const Symbols = () => {
         const response = await axios.get(
           `https://api.forexrateapi.com/v1/symbols?api_key=${API_KEY}`
         );
-        handleSetSymbols(response.data.symbols);
-        localStorage.setItem("symbols", JSON.stringify(response.data.symbols));
+        if (response.data.success) {
+          handleSetSymbols(response.data.symbols);
+        }
       } catch (error) {
         console.error("Error fetching symbols:", error);
       }
     };
 
-    // fetchSymbols();
+    fetchSymbols();
   }, []);
 
   return (
